b44b03339453d1a57c3ba04c2b8d3fae32a67aca
Jason Snider <root@jasonsnider.com>
2019-02-13 16:06:00 -0600

    Added remaingin CRUD pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,10 @@ const routes: Routes = [
   { path: 'logout', loadChildren: './logout/logout.module#LogoutPageModule' },
   { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
   { path: 'users', loadChildren: './users/users.module#UsersPageModule' },
-  { path: 'user/:id', loadChildren: './user/user.module#UserPageModule' }
+  { path: 'user/:id', loadChildren: './user/user.module#UserPageModule' },
+  { path: 'user-create', loadChildren: './user-create/user-create.module#UserCreatePageModule' },
+  { path: 'user-edit/:id', loadChildren: './user-edit/user-edit.module#UserEditPageModule' },
+  { path: 'user-delete/:id', loadChildren: './user-delete/user-delete.module#UserDeletePageModule' }
 ];
 
 @NgModule({
---
+++ src/app/user-delete/user-delete.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-delete',
+  templateUrl: './user-delete.page.html',
+  styleUrls: ['./user-delete.page.scss'],
+})
+export class UserDeletePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
