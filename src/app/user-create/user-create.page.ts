25caf1e19666f03af9c79aa2b94fe58903c8084d
Jason Snider <root@jasonsnider.com>
2019-02-13 17:05:20 -0600

    Added the ability to create a user

--- src/app/user-create/user-create.page.ts
+++ src/app/user-create/user-create.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'; 
+import { Router } from '@angular/router';
+
+import { UsersService } from '../users.service';
+import { User } from '../user';
+
 
 @Component({
   selector: 'app-user-create',
@@ -7,9 +13,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserCreatePage implements OnInit {
 
-  constructor() { }
+  user:User = new User();
+  errors: any = {};
 
-  ngOnInit() {
+  constructor(
+    private usersService: UsersService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {}
+
+  response(response): void{
+    
+    if(response.success==false){
+
+      if( response.error.errors.name == 'MissingUsernameError' ){
+        this.errors.username = 'Please enter a username';
+      }
+
+      if( response.error.errors.name == 'UserExistsError' ){
+        this.errors.username = 'A user with the given username is already registered';
+      }
+
+      if( response.error.errors.email ){
+        this.errors.email = response.error.errors.email.message;
+      }
+
+    }
+
+    if(response.success===true){
+      this.router.navigate(['/user', response.user._id]);
+    }
+  }
+
+  onSubmit(): void{
+    this.usersService.create(this.user).subscribe(
+      (response) => {
+        this.response(response);
+      }
+    );
   }
 
 }
--- src/app/user-edit/user-edit.page.ts
+++ src/app/user-edit/user-edit.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'; 
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { UsersService } from '../users.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-user-edit',
@@ -7,9 +12,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEditPage implements OnInit {
 
-  constructor() { }
+
+  user:User;
+  errors: any = {};
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private usersService: UsersService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe((params)=>{
+      this.getUser(params['id']);
+    });
   }
+
+  response(response): void{
+    
+    if(response.success==false){
+
+      if( response.error.errors.name == 'MissingUsernameError' ){
+        this.errors.username = 'Please enter a username';
+      }
+
+      if( response.error.errors.name == 'UserExistsError' ){
+        this.errors.username = 'A user with the given username is already registered';
+      }
+
+      if( response.error.errors.email ){
+        this.errors.email = response.error.errors.email.message;
+      }
+
+    }
+
+    if(response.success===true){
+      this.router.navigate(['/user', response.user._id]);
+    }
+  }
+
+  onSubmit(): void{
+    this.usersService.edit(this.user).subscribe(
+      (response) => {
+        this.response(response);
+      }
+    );
+  }
+
+  getUser(id:string): void{
+    this.usersService.user(id).subscribe(
+      (response:any)=>{
+        this.user = response.user;
+      }
+    );
+  }
+
 
 }
